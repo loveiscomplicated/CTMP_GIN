7d97101c683217975fa6dcbea4f96d2c34c7977b
loveiscomplicated <yyjsjsjs243@gmail.com>
2026-01-04 14:35:33 -0500

    [ADD] - instead, we need to process mi_dict whenever the random seed changes (because train data changes)

---
+++ data_processing/process_mi_dict.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+import torch
+from sklearn.feature_selection import mutual_info_classif
+
+cur_dir = os.path.dirname(__file__)
+
+def get_mi_dict(train_data, seed):
+    pass
+
+def get_mi_dict_static(train_data, seed):
+    pass
+
+
+
+
