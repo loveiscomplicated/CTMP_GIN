7db51e97b33cfa6c47c62a98d3abbcde67bd415c
loveiscomplicated <yyjsjsjs243@gmail.com>
2026-01-18 18:15:08 +0900

    [REFACTOR] - main.py

--- src/main.py
+++ src/main.py
@@ -1,25 +1,35 @@
 import os
+import sys
+from pathlib import Path
+
+# Add the project root to the Python path
+project_root = Path(__file__).resolve().parent
+sys.path.insert(0, str(project_root.parent))
+
 import yaml
 import argparse
 import pandas as pd
-from data_processing.tensor_dataset import TEDSTensorDataset, TEDSDatasetForGIN
-from data_processing.data_utils import train_test_split_stratified
-from data_processing.process_mi_dict import search_mi_dict
-from data_processing.edge import mi_edge_index_batched, mi_edge_index_batched_for_baseline
-from models.factory import build_model
-from utils.experiment import make_run_id, ensure_run_dir, ExperimentLogger
-from utils.seed_set import set_seed
+from src.data_processing.tackle_missing_value import tackle_missing_value_main
+from src.data_processing.tensor_dataset import TEDSTensorDataset, TEDSDatasetForGIN
+from src.data_processing.data_utils import train_test_split_stratified
+from src.data_processing.process_mi_dict import search_mi_dict
+from src.data_processing.edge import mi_edge_index_batched, mi_edge_index_batched_for_baseline
+from src.models.factory import build_model
+from src.utils.experiment import make_run_id, ensure_run_dir, ExperimentLogger
+from src.utils.seed_set import set_seed
 
 def parse_args():
     p = argparse.ArgumentParser()
-    p.add_argument("--config", type=str, required=True)
+    p.add_argument("--config", type=str, required=True) # config file location
     # overrides
-    p.add_argument("--model", type=str, default=None)
+    # p.add_argument("--model", type=str, default=None) no need, model selection only based on config
+    # more detailed adadjustment able in config file
     p.add_argument("--batch_size", type=int, default=None)
     p.add_argument("--lr", type=float, default=None)
     p.add_argument("--epochs", type=int, default=None)
     p.add_argument("--seed", type=int, default=None)
-    p.add_argument("--binary", type=int, default=None)
+    # TODO non-binary (Multiclass Classification) implement later
+    # p.add_argument("--binary", type=int, default=None)
     return p.parse_args()
 
 def load_yaml(path: str) -> dict:
@@ -89,16 +99,28 @@ def main():
         logger=logger,
     )
 
+    cur_dir = os.path.dirname(__file__)
+    run_dir = os.path.join(cur_dir, 'runs')
+    root = os.path.join(cur_dir, 'data')
 
 
-
-cur_dir = os.path.dirname(__file__)
-run_dir = os.path.join(cur_dir, 'runs')
-root = os.path.join(cur_dir, 'data')
-
-
-run_id = make_run_id(cfg)
-run_dir = ensure_run_dir("runs", run_id)
-logger = ExperimentLogger(cfg, run_dir)
+    run_id = make_run_id(cfg)
+    run_dir = ensure_run_dir("runs", run_id)
+    logger = ExperimentLogger(cfg, run_dir)
 
 
+    CUR_DIR = os.path.dirname(__file__)
+    raw_df_path = os.path.join(CUR_DIR, 'data', 'raw', 'TEDS_Discharge.csv')
+
+    # missing_corrected.csv will be created after `tackle_missing_value_main`.
+    # missing_corrected.csv is used in every training processes.
+    missing_corrected_path = os.path.join(CUR_DIR, 'data', 'raw', 'missing_corrected.csv')
+    root = os.path.join(CUR_DIR, 'data')
+
+    missing_corrected = tackle_missing_value_main(raw_df_path, missing_corrected_path)
+
+    # TODO make dataset class
+
+
+if __name__ == "__main__":
+    main()
