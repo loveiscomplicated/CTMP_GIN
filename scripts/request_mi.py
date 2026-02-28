bcc90762f3c972a2bd2c15c24f3a6a2edd0f3dbe
loveiscomplicated <yyjsjsjs243@gmail.com>
2026-03-01 01:53:41 +0900

    [UPDATE] - use mi_worker for edge building

--- scripts/request_mi.py
+++ scripts/request_mi.py
@@ -12,6 +12,7 @@ DATASET_ID = "tedsd_2022"
 REMOTE_BASE = "gdrive:CTMP_GIN_mi_service"
 
 LOCAL_CACHE_DIR = Path("/workspace/CTMP_GIN/cache/mi_dict")
+# LOCAL_CACHE_DIR = Path(".") # for debugging
 LOCAL_CACHE_DIR.mkdir(parents=True, exist_ok=True)
 
 
