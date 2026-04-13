cdc2dcbdeb82782aec1c0ae4c36cba5c53a38f36
loveiscomplicated <yyjsjsjs243@gmail.com>
2026-04-13 11:07:43 +0900

    [FIX] config

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ def parse_args():
     p.add_argument("--seed", type=int, default=None)
     p.add_argument("--decision_threshold", type=float, default=None)
     p.add_argument("--binary", type=int, default=None)
-    p.add_argument("--cv", type=lambda x: x.lower() not in ("false", "0", "no"), default=True)
+    p.add_argument("--cv", type=lambda x: x.lower() not in ("false", "0", "no"), default=None)
     return p.parse_args()
 
 
